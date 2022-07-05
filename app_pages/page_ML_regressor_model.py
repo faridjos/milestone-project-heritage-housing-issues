6e8b35cd2074639748e00563d682b95ca2778ecc
faridjos <farid.benachenhou@hotmail.com>
2022-07-05 09:12:16 +0000

    add text describing the plots in ML Regressor Model page

--- app_pages/page_ML_regressor_model.py
+++ app_pages/page_ML_regressor_model.py
@@ -35,6 +35,7 @@ def page_ML_regressor_model_body():
 
     # show best features and their importance for the ML model
     st.write("* **The features the model was trained on and their importance**")
+    st.info("We see that the most important variable for predicting the sale price is 'OverallQual'")
     st.write(X_train.columns.to_list())
     st.image(house_price_feat_importance)
     st.write("---")
@@ -46,9 +47,11 @@ def page_ML_regressor_model_body():
      
     # Plot predicted versus actual sale price for train and test sets
     st.write("* **Predicted versus actual sale price scatterplot**")
-    st.info("* We notice that our model may not accurately predict prices above $400000. "
-    "This can be seen on the scatterplot showing the predictions on the test set (right plot below). "
-    "At these prices the datapoints are few but they do not follow the red line. "
+    st.info("* For prices below $400000, the data points follow the red line where the predicted price"
+    " equals actual price.  \n"
+    "* For higher prices, our model may not accurately predict prices. "
+    "On the scatterplot showing the predictions on the train set (left plot below), "
+    "the prices above $400000 are underestimated (the data points are below the red line)"
     )
     regression_evaluation_plots(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, pipeline=pipeline, alpha_scatter=0.5)
 
